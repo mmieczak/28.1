4d2f92d0d65dad3457b343e5b0819b6f42d1b773
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-07-16 14:06:18 +0200

    Empty project

---
+++ src/test/java/pl/mmieczak/exercises/ExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mmieczak.exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
